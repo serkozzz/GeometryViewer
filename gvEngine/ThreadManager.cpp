025452ec2d2f1d6513fd65b5017662b4e2c381bb
Sergey <s.kozlov@blackmana.com>
2016-01-21 19:20:21 +0300

    wip

---
+++ gvEngine/ThreadManager.cpp
@@ -0,0 +1,30 @@
+#include <chrono>
+#include <thread>
+
+
+
+#include "ThreadManager.h"
+
+using namespace gv::Engine;
+
+ThreadManager::ThreadManager(std::shared_ptr<IRenderer> renderer, 
+							 std::shared_ptr<IInputController> inputController, 
+							 float inputCheckInterval,
+							 float renderLoopInterval) 
+							 : _renderer(renderer), _inputController(inputController), _inputCheckInterval(inputCheckInterval), _renderLoopInterval(renderLoopInterval)
+{
+
+}
+
+void ThreadManager::start()
+{
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	std::chrono::system_clock::time_point nextRender = now + _renderLoopInterval;
+	std::chrono::system_clock::time_point nextInputCheck = now + _inputCheckInterval;
+
+	do
+	{
+
+	}
+	while(true);
+}
--- gvEngine/ThreadManager.h
+++ gvEngine/ThreadManager.h
@@ -9,15 +9,24 @@ namespace gv
 {
 	namespace Engine
 	{
-		
+
 		class ThreadManager
 		{
 			std::shared_ptr<IRenderer> _renderer;
-			std::shared_ptr<IInputController> _inputController;			
+			std::shared_ptr<IInputController> _inputController;		
+
+			std::chrono::duration<double, std::milli> _inputCheckInterval;
+			std::chrono::duration<double, std::milli> _renderLoopInterval;
+			std::chrono::system_clock::time_point _lastRenderTime;
+
 
 		public:
-			ThreadManager(std::shared_ptr<IRenderer> renderer, std::shared_ptr<IInputController> _inputController);
-
+			///all intervals in ms
+			ThreadManager(std::shared_ptr<IRenderer> renderer, 
+				std::shared_ptr<IInputController> inputController, 
+				float inputCheckInterval,
+				float renderLoopInterval);
+			void start();
 		};
 	}
 }
